7b5663567b894b4cf591f1b94cfd441de1ed6189
Omar El Yousfi <elyousfiomarr@gmail.com>
2025-05-11 20:45:08 -0300

    new user summary

--- app.py
+++ app.py
@@ -55,25 +55,31 @@ with st.form("Loan form"):
     desirable_loan = st.number_input("Enter Desirable Loan Amount", min_value=0, step=1000, format="%d")
     st.form_submit_button("Check Standing")
 
+special_id = uuid.uuid1()
+
 if desirable_loan < 100:
     st.warning("We accept loans starting from $100. Please enter a valid amount.")
     st.stop()
+elif desirable_loan > 50000:
+    st.success("We accept automatic loans up to $50,000. Please enter a valid amount.")
+    st.stop()
+
 if account_number and desirable_loan > 0:
-    special_id = uuid.uuid1()
+    
     user_row = user_summary[user_summary['primary_account_number'].astype(str) == account_number]
     if not user_row.empty:
         user_data = all_transactions[all_transactions['primary_account_number'].astype(str) == account_number]
         st.header(f"Welcome {user_data['cardholder_name'].values[0]}")
         st.divider()
-        standing = user_row['predicted_standing'].values[0]
+        standing = user_row['standing'].values[0]
         if standing.upper() == 'LOW' and desirable_loan > 2000:
-            st.warning(f"You won't be able to get a loan of ${desirable_loan}. For more details, please contact an agent with this id: {special_id}")
+            st.warning(f"You won't be able to get a loan of ${desirable_loan}. For more details, please contact an agent.")
         elif standing.upper() == 'MEDIUM' and desirable_loan > 15000:
-            st.warning(f"Sorry, it seems you won't be able to get a loan of ${desirable_loan}. For more details, please contact an agent with this id: {special_id}")
+            st.warning(f"Sorry, it seems you won't be able to get a loan of ${desirable_loan}. For more details, please contact an agent.")
         elif standing.upper() == 'PLATINUM':
-            st.success(f"🏅Congrats! You are able to get this loan amount, please contact an agent with this id: {special_id}")
+            st.success(f"🏅Congrats! You are able to get this loan amount, please contact an agent.")
         else:
-            st.success(f"Congrats! You are able to get a ${desirable_loan} loan. For more information, please contact an agent with this id: {special_id}")
+            st.success(f"Congrats! You are able to get a ${desirable_loan} loan.")
 
         with st.expander("Show my transactions"):
             st.dataframe(user_data[["acquirer_reference_number", "transaction_type", "transaction_amount", "transaction_currency_code", "merchant_category_code", "card_acceptor_id", "merchant_name", "transaction_date"]].sort_values(by="transaction_date", ascending=False).reset_index(drop=True), use_container_width=True)
